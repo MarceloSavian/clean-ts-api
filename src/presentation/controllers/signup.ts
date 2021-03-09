92a24f78cc6eec9856873307e593c188f88ac5ad
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-09 17:44:19 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name')
+        body: new MissingParamError('name')
       }
     }
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: email')
+        body: new MissingParamError('email')
       }
+    }
+
+    return {
+      statusCode: 400,
+      body: new MissingParamError('email')
     }
   }
 }
