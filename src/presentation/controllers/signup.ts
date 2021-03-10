657d4d6ee9ff9a68397b000a5e77f866295cb3d8
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-09 21:32:29 -0300

    feat: ensure SignUpController returns 400 if password confirmation is invalid

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -11,6 +11,7 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
+      const { email, password, passwordConfirmation } = httpRequest.body
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
       for (const field of requiredFields) {
@@ -19,7 +20,11 @@ export class SignUpController implements Controller {
         }
       }
 
-      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
+      if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError('email'))
       }
 
