2fb10aa9c990801002667140220be40946009e38
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-12 18:06:31 -0300

    feat: ensure LogControllerDecorator calls controller with correct values

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,25 +1,43 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+interface SutInterface {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        body: {
+          email: 'any_mail@mail.com',
+          name: 'any_name',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        },
+        statusCode: 200
+      }
+      return Promise.resolve(httpResponse)
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutInterface => {
+  const controllerStub = makeController()
+  return {
+    sut: new LogControllerDecorator(controllerStub),
+    controllerStub
+  }
+}
+
 describe('Log Controller Decorator', () => {
   test('Shoul call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          body: {
-            email: 'any_mail@mail.com',
-            name: 'any_name',
-            password: 'any_password',
-            passwordConfirmation: 'any_password'
-          },
-          statusCode: 200
-        }
-        return Promise.resolve(httpResponse)
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
+
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
+
     const httpRequest: HttpRequest = {
       body: {
         email: 'any_mail@mail.com',
@@ -28,7 +46,9 @@ describe('Log Controller Decorator', () => {
         passwordConfirmation: 'any_password'
       }
     }
+
     await sut.handle(httpRequest)
+
     expect(handleSpy).toHaveBeenLastCalledWith(httpRequest)
   })
 })
