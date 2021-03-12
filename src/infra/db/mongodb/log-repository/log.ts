d9f06f358ab2728424f140555850e2f9266e20a9
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-12 20:58:19 -0300

    refactor: corret repository name

--- src/infra/db/mongodb/log-repository/log.ts
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -1,4 +1,4 @@
-import { LogErrorRepository } from '../../../../data/protocols/log-error-repository copy'
+import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
 import { mongoHelper } from '../helpers/mongo-helper'
 
 export class LogMongoRepository implements LogErrorRepository {
