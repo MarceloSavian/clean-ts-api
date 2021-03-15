6b1de40044a0ea9df306768883b32caa77aba1af
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-15 15:06:10 -0300

    test: ensure updateAccessToken updates the account when succeds

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,18 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+  test('Should update the account accessToken on UpdateAccessToken success', async () => {
+    const { sut } = makeSut()
+    const result = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(result.ops[0]?.accessToken).toBeFalsy()
+    const id = result.ops[0]._id
+    await sut.updateAccessToken(id, 'any_token')
+    const account = await accountCollection.findOne({ _id: result.ops[0]._id })
+    expect(account).toBeTruthy()
+    expect(account?.accessToken).toBeTruthy()
+  })
 })
