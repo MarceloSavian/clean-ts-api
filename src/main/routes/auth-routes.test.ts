55abb9a7290d8dc7d150aee96d05f7d711584785
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-16 22:38:52 -0300

    test: add LoginRoute

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -1,6 +1,9 @@
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { mongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+
+let accountCollection: Collection
 
 describe('Auth routes', () => {
   beforeAll(async () => {
@@ -10,7 +13,7 @@ describe('Auth routes', () => {
     await mongoHelper.disconnect()
   })
   beforeEach(async () => {
-    const accountCollection = await mongoHelper.getCollection('accounts')
+    accountCollection = await mongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -20,9 +23,28 @@ describe('Auth routes', () => {
         .post('/api/signup')
         .send({
           name: 'Marcelo',
-          email: 'marcelo.savian@gmail.com',
+          email: 'teste@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Marcelo',
+          email: 'teste@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'teste@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
