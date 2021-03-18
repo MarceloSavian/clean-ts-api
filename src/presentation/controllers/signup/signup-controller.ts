04a7801a4087dd0f45a3c4fa0cc3ad6041ce853d
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-18 20:00:08 -0300

    feat: Ensure SignupController calls auth with correct values

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,9 +1,13 @@
 import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
 import { Validation } from '../../protocols/validation'
-import { Controller, HttpRequest, HttpResponse, AddAccount } from './signup-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, AddAccount, Authentication } from './signup-controller-protocols'
 
 export class SignUpController implements Controller {
-  constructor (private readonly addAccount: AddAccount, private readonly validation: Validation) { }
+  constructor (
+    private readonly addAccount: AddAccount,
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -16,6 +20,11 @@ export class SignUpController implements Controller {
 
       const account = await this.addAccount.add({
         name,
+        email,
+        password
+      })
+
+      await this.authentication.auth({
         email,
         password
       })
