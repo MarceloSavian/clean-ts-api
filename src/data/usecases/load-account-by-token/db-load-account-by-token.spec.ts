03335fa5030d5aea509d9e2bdf07b0232cba5258
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-19 19:32:47 -0300

    test: ensure DbLoadAccountByToken returns an account on success

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -63,4 +63,10 @@ describe('DbLoadAccountByTokent UseCase', () => {
     await sut.load('any_token', 'any_role')
     expect(loadByTokenSpy).toHaveBeenLastCalledWith('any_token', 'any_role')
   })
+  test('Should return LoadAccountByTokentRepository if Decrypter returns null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(Promise.resolve(null))
+    const accounnt = await sut.load('any_token', 'any_role')
+    expect(accounnt).toBeNull()
+  })
 })
