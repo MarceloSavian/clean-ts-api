c013487c59ac11a252f87b563f73930edbc48292
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-19 15:36:11 -0300

    feat: ensure DbAddSurvey calls AddSurveyRepository with correct values

---
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -0,0 +1,13 @@
+import { AddSurvey, AddSurveyModel } from '../../../domain/usecases/add-survey'
+import { AddSurveyRepository } from './db-add-survey-protocols'
+
+export class DbAddSurvey implements AddSurvey {
+  constructor (
+    private readonly addSurveyRepository: AddSurveyRepository
+  ) {}
+
+  async add (data: AddSurveyModel): Promise<null> {
+    await this.addSurveyRepository.add(data)
+    return Promise.resolve(null)
+  }
+}
