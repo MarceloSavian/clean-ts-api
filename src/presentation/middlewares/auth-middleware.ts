ced8fa5407aa33fac8740d60cbd4f8790c64b272
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-19 18:25:56 -0300

    feat: add role to AuthMiddleware constructor

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -6,7 +6,8 @@ import { Middleware } from '../protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
   constructor (
-    private readonly loadAccountByToken: LoadAccountByToken
+    private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly role?: string
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -14,7 +15,7 @@ export class AuthMiddleware implements Middleware {
       const accesToken = httpRequest?.headers?.['x-access-token']
       if (!accesToken) return forbidden(new AccessDeniedError())
 
-      const account = await this.loadAccountByToken.load(httpRequest.headers['x-access-token'])
+      const account = await this.loadAccountByToken.load(httpRequest.headers['x-access-token'], this.role)
       if (!account) return forbidden(new AccessDeniedError())
 
       return ok({ accountId: account.id })
