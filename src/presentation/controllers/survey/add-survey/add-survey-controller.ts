9cbcfff6656cda983cf2fb9564b5a076ef2670b2
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-19 14:33:01 -0300

    feat: ensure AddSurveyCOntrolller calls Validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,16 @@
+import { badRequest } from '../../../helpers/http/http-helper'
+import { Validation } from '../../../protocols'
+import { Controller, HttpRequest, HttpResponse } from './add-surver-controller-protocols'
+
+export class AddSurveyController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest.body)
+    if (error) return badRequest(error)
+
+    return Promise.resolve({ statusCode: 200, body: {} })
+  }
+}
