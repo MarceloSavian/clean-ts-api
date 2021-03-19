8881c2efe0d6c6ede8053f340365ccd74f6053bb
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-19 16:32:40 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,39 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { mongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveyCollection: Collection
+
+describe('Auth routes', () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(String(process.env.MONGO_URL))
+  })
+  afterAll(async () => {
+    await mongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    surveyCollection = await mongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'image'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
