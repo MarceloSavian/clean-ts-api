d45d3a611271e933c280b19cd903db7c5b02d856
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-19 17:44:46 -0300

    feat: ENsure AuthMiddleware calls LoadAccountByToken with correct values

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,10 +1,16 @@
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers/http/http-helper'
-import { HttpResponse } from '../protocols'
+import { HttpRequest, HttpResponse } from '../protocols'
 import { Middleware } from '../protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
-  async handle (): Promise<HttpResponse> {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadAccountByToken.load(httpRequest.headers['x-access-token'])
     return Promise.resolve(forbidden(new AccessDeniedError()))
   }
 }
