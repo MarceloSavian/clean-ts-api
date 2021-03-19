ba420b1bacc3a699dc0f6acd6f21d4996ba5bcc4
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-18 21:58:00 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../presentation/helpers/validators'
-import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../presentation/helpers/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
 import { makeLoginValidation } from './login-validation-factory'
 
-jest.mock('../../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../../presentation/helpers/validators/validation-composite')
 
 describe('Login validation', () => {
   test('Should call ValidationComposite with all validations', () => {
---
+++ src/main/factories/usecases/authentication/db-authentication-factory.ts
@@ -0,0 +1,13 @@
+import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
+import { Authentication } from '../../../../domain/usecases/authentication'
+import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../../config/env'
+
+export const makeDbAuthentication = (): Authentication => {
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const bcryptAdapter = new BcryptAdapter(12)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+}
