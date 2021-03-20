312c57de98282bf1f6972ec92e89ccd6a52dfe2d
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-19 21:45:18 -0300

    refactor: move Factories to a folder

--- src/main/factories/controllers/auth/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/auth/login/login-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
-import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../../validation/validators'
+import { Validation } from '../../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../../../infra/validators/email-validator-adapter'
 import { makeLoginValidation } from './login-validation-factory'
 
-jest.mock('../../../../validation/validators/validation-composite')
+jest.mock('../../../../../validation/validators/validation-composite')
 
 describe('Login validation', () => {
   test('Should call ValidationComposite with all validations', () => {
