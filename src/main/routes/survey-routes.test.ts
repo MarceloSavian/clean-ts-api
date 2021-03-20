a2b7b083125b7a72a176b38ef93e153c81661eae
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-19 22:49:51 -0300

    test: ensure AuthMiddleware return 204 if access-token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import { mongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Auth routes', () => {
   beforeAll(async () => {
@@ -15,6 +18,8 @@ describe('Auth routes', () => {
   beforeEach(async () => {
     surveyCollection = await mongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await mongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -35,5 +40,38 @@ describe('Auth routes', () => {
         })
         .expect(403)
     })
+  })
+  test('Should return 204 on survey with access-token', async () => {
+    const res = await accountCollection.insertOne({
+      name: 'Test',
+      emai: 'test@gmail.com',
+      password: 'any_passowrd',
+      role: 'admin'
+    })
+    const id = res.ops[0]._id
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne({
+      _id: id
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'image'
+          },
+          {
+            answer: 'Answer 2'
+          }
+        ]
+      })
+      .expect(204)
   })
 })
