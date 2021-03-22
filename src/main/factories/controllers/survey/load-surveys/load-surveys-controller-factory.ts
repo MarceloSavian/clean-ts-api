acc60dd3fe8d932aaf6e5d7fc61d095d1fb7a8e1
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-22 17:16:33 -0300

    feat: ensure SaveSurveyResult returns 403 without token

--- src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@/presentation/protocols'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
 import { LoadSurveysController } from '@/presentation/controllers/survey/load-surveys/load-surveys-controller'
-import { makeDbLoadSurvey } from '../../../usecases/survey/load-surveys/db-load-surveys'
+import { makeDbLoadSurvey } from '../../../usecases/survey/load-surveys/db-load-surveys-factory'
 
 export const makeLoadSurveyController = (): Controller => {
   const loadSurveyController = new LoadSurveysController(makeDbLoadSurvey())
