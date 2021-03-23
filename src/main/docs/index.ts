d4efcc242d17c5bc4fb61e8ceecf3f05fea48576
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-23 11:06:24 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-paths'
+import { accountSchema } from './schemas/account-schema'
+import { loginSchema } from './schemas/login-schema'
+
 export default {
-  swagger: '2.0',
+  openapi: '3.0.0',
   info: {
     title: 'Clean Node Api',
     description: 'API from Mango curse to realize surveys',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    'login-params': loginSchema
   }
 }
