9492cfa4789b74ae7c4981ca078f5e76137e4811
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-24 18:04:48 -0300

    test: ensure LoadSurveyResult returns 403 without token

---
+++ src/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory.ts
@@ -0,0 +1,9 @@
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveyResult } from '@/main/factories/usecases/survey-result/load-survey-result/db-load-survey-result-factory'
+import { LoadSurveyResultController } from '@/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller'
+import { Controller } from '@/presentation/protocols'
+
+export const makeLoadSurveyResultController = (): Controller => {
+  const loadSurveyController = new LoadSurveyResultController(makeDbLoadSurveyResult())
+  return makeLogControllerDecorator(loadSurveyController)
+}
